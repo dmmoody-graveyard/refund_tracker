c967959a2ec476165dced592f46e73b7f534f0f1
Duane M. Moody <dmmoody@gmail.com>
2015-02-05 14:24:12 -0800

    create member table columns

--- db/migrate/20150205221838_create_member_tables.rb
+++ db/migrate/20150205221838_create_member_tables.rb
@@ -1,4 +1,22 @@
 class CreateMemberTables < ActiveRecord::Migration
   def change
+    create_table :cases do |t|
+      t.string :name
+      t.string :number
+    end
+
+    create_table :certs do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :member_id
+      t.integer :case_id
+    end
+
+    create_table :claimants do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :telephone
+      t.integer :cert_id
+    end
   end
 end
