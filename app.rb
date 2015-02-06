bebba2d94672d40457d2f8d67e01043c36a73cd3
Duane M. Moody <dmmoody@gmail.com>
2015-02-06 13:49:57 -0800

    add group page and new group page

--- app.rb
+++ app.rb
@@ -6,3 +6,19 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get '/' do
   erb :index
 end
+
+get '/case' do
+  @case = Case.all
+  erb :case
+end
+
+get '/case/new' do
+  erb :new_case
+end
+
+post '/case' do
+  name = params.fetch 'name'
+  number = params.fetch 'number'
+  @case = Case.create :name => name, :number => number
+  redirect '/case'
+end
