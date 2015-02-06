0d2f92cb122df9fb41c5429fa4c1d7a95d8616c7
Duane M. Moody <dmmoody@gmail.com>
2015-02-06 11:28:55 -0800

    add refund association

--- lib/claimant.rb
+++ lib/claimant.rb
@@ -1,3 +1,4 @@
 class Claimant < ActiveRecord::Base
   belongs_to :cert
+  has_many :refund
 end
--- spec/claimant_spec.rb
+++ spec/claimant_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Claimant do
   it{ should belong_to :cert }
+  it{ should have_many :refund }
 end
